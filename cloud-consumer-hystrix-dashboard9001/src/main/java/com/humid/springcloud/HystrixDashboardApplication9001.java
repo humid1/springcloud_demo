069bdf319261ec4e0278db4bfcbd0c8bca58a45c
qiujianping <qiujianp123@163.com>
2021-05-26 17:44:42 +0800

    使用hystrix-dashboard

---
+++ cloud-consumer-hystrix-dashboard9001/src/main/java/com/humid/springcloud/HystrixDashboardApplication9001.java
@@ -0,0 +1,17 @@
+package com.humid.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+/**
+ * @author qiujianping
+ * @date Created in 2021/5/25 11:04
+ */
+@SpringBootApplication
+@EnableHystrixDashboard
+public class HystrixDashboardApplication9001 {
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixDashboardApplication9001.class, args);
+    }
+}
